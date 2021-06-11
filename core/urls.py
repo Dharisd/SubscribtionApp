5debb1f71abdec0b4720662f00623d54f1a841b3
dharisd <dharisd@gmail.com>
2021-06-11 17:35:11 +0000

    added initial service details

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from core.views.DashBoardViews import DashBoardView
 from core.views.SubscriberViews import CreateSubscriberView,ListSubscribersView
-from core.views.SubscribtionServiceViews import ListSubscribtionServices
+from core.views.SubscribtionServiceViews import ListSubscribtionServices,DetailSubscribtionServicesView
 
 
 app_name = "subscribtion-app"
@@ -13,6 +13,10 @@ urlpatterns = [
     path('list/subscribers', ListSubscribersView.as_view(), name='list-subscribers'),
     #create views
     path('create/subscribers', CreateSubscriberView.as_view(), name='create-subscribers'),
+
+    #detail views
+    path('details/<int:pk>', DetailSubscribtionServicesView.as_view(), name='detail-services'),
+
 
 
 ]
--- core/views/SubscribtionServiceViews.py
+++ core/views/SubscribtionServiceViews.py
@@ -7,9 +7,18 @@ from django.views.generic.edit import CreateView
 
 
 
+
 class ListSubscribtionServices(ListView):
     template_name = "core/list_services.html"
     context_object_name = "services"
+    model = SubscribtionService
+
+
+
+
+class DetailSubscribtionServicesView(DetailView):
+    template_name = "core/detail_services.html"
+    context_object_name = "service"
     model = SubscribtionService
 
 
